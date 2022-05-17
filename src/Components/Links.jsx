46db12ea7c4a43f0c802b41ca62ce98bb570179f
Deepak yadav <deepaky541.dy@gmail.com>
2022-05-17 13:21:49 +0530

    Update Links.jsx

--- src/Components/Links.jsx
+++ src/Components/Links.jsx
@@ -1,11 +1,10 @@
 import './Links.css'
 export const Links=()=>{
     const links=['Services','Projects','About'];
-   const map1=links.map(x=>x+" ");
     return(
         <div className="links">
         <a href='deepak'>
-        {map1}
+        {links.map(el=>el+" ")}
         </a>
         </div>
     );
